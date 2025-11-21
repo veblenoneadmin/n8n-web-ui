8c5b07b024563f680fe4942293b5bdb8e6fc47b3
veblenoneadmin <veblengroup.com.au>
2025-11-21 11:36:01 +0800

    auth update

--- auth.php
+++ auth.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["user_id"])) {
-    header("Location: login.php");
-    exit();
+$current = basename($_SERVER['PHP_SELF']);
+
+// Pages that must NOT require login
+$public_pages = ['login.php', 'check_session.php'];
+
+if (!in_array($current, $public_pages)) {
+    if (!isset($_SESSION["user_id"])) {
+        header("Location: login.php");
+        exit();
+    }
 }
